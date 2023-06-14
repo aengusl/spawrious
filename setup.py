d2d705aa3a2950c803bad9c55fe276c6ea304265
Aengus Lynch <37474130+aengusl@users.noreply.github.com>
2023-06-14 15:13:49 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+  name="spawrious",
+  version="0.1.0",
+  description="A package for generating random data",
+  author="Aengus Lynch et al",
+  author_email="aengus.lynch.17@ucl.ac.uk",
+  url="https://github.com/aengusl/spawrious/new/main",
+  packages=["spawrious"],
+  install_requires=["numpy"],
+  license="CC-BY-SA 4.0",
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: Creative Commons Attribution-Share Alike 4.0 International License",
+    "Operating System :: OS Independent",
+  ],
+)
